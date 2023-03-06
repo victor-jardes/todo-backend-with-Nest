3408a50d4ad6ad6127eacf6a94d6f15b17a2c5aa
victor <victor_maanoel@outlook.com>
2023-03-06 18:34:25 -0400

    refactor(file location): change faile location or delete file

--- src/app/user/memory/index.ts
+++
@@ -1,6 +0,0 @@
-import { CreateUserDTO } from '../dto/create-user.dto';
-
-export abstract class MemoryInDatabase {
-  abstract create(body: CreateUserDTO): void;
-  abstract findAll(): CreateUserDTO[];
-}
